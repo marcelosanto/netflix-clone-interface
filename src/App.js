25223554cbd29f7bdb571a799ce4ac16e63850b0
MarceSanto <rocha.mer21@gmail.com>
2020-09-10 17:41:38 -0300

    iniciando o Featured Filme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import Tmdb from './Tmdb'
+
+import MovieRow from './components/MovieRow.js'
+import FeaturedMovie from './components/FeaturedMovie'
 
 function App() {
-  return <div>Oie Mundo</div>
+  const [movieList, setMovieList] = useState([])
+  const [featuredData, setFeaturedData] = useState([])
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista ToTal
+      let list = await Tmdb.getHomeList()
+      setMovieList(list)
+
+      // Pegando o Featured
+      let originals = list.filter((i) => i.slug === 'originals')
+      let randomChosen = Math.floor(
+        Math.random() * (originals[0].items.results.length - 1)
+      )
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+    }
+
+    loadAll()
+  }, [])
+
+  return (
+    <div className='page'>
+      {featuredData && <FeaturedMovie item={featuredData} />}
+      <section className='lists'>
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+    </div>
+  )
 }
 
 export default App
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -79,4 +79,25 @@ export default {
       },
     ]
   },
+  getMovieInfo: async (movieId, type) => {
+    let info = {}
+
+    if (movieId) {
+      // eslint-disable-next-line default-case
+      switch (type) {
+        case 'movie':
+          info = await basicFetch(
+            `/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`
+          )
+          break
+        case 'tv':
+          info = await basicFetch(
+            `/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`
+          )
+          break
+      }
+    }
+
+    return info
+  },
 }
